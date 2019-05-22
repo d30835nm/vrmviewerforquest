7106b7749cb8748e9e2d0592bf97b6962605f1f3
bnut_yashinut <mizota@bnut.jp>
2019-05-22 12:59:03 +0900

    メニューの表示非表示切り替え機能

---
+++ Assets/VrmViewerForQuest/Scripts/VrmMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace VrmViewer
+{
+    public class VrmMenu : MonoBehaviour
+    {
+        bool visible = true;
+
+        void Update()
+        {
+            //Questの場合はStartボタン、Goの場合はBackボタンをメニューボタンとする
+            if (OVRInput.GetDown(OVRInput.Button.Start) || OVRInput.GetDown(OVRInput.Button.Back))
+            {
+                visible = !visible;
+                ChangeVisibility(visible);
+            }
+        }
+
+        void ChangeVisibility(bool visible)
+        {
+            var mainCamera = Camera.main.transform;
+            var targetRotation = Quaternion.Euler(new Vector3(0f, mainCamera.rotation.eulerAngles.y, 0f));
+            transform.SetPositionAndRotation(mainCamera.position, targetRotation);
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(visible);
+            }
+        }
+    }
+}
